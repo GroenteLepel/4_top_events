be9372ff2ea1e04e1ae3fec08fd81e1d5c0a5029
Daniël Kok <d.kok@student.science.ru.nl>
2020-02-06 09:48:22 +0100

    Fixed bug where 100 percent would still leave a blank space.

--- progress_bar/progress_bar.py
+++ progress_bar/progress_bar.py
@@ -1,9 +1,8 @@
 def percentage_to_bar(percentage: float):
     int_per = int(percentage)
     res = int_per % 5
-    done = (int_per - res)
-    to_do = 100 - done
-    done = int(done / 5)
-    to_do = int(to_do / 5)
-    bar = "|" + int(done) * "█" + int(to_do) * "_" + "|" + "{0:3.0f}%".format(percentage)
+    done = int(((int_per - res) + 1) / 5)
+    to_do = int((100 - done) / 5)
+    bar = "|" + int(done) * "█" + int(to_do) * "_" + "|" + \
+          "{0:3.0f}%".format(percentage)
     return bar
