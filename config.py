ee9e1b98c9284bbf308ae2eb6317b4be40b7c30b
GroenteLepel <d.kok@student.science.ru.nl>
2020-02-11 10:54:55 +0100

    Added constants for constructing the event map.

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
 PICKLEJAR = "data/picklejar/"
+N_BINS = 8  # number of bins per particle in the stored event map
+LEN_VECTOR = 5  # length of the four-vector plus the charge element
+N_PARTICLES = 5  # amount of particles in the datafile, j, b, g, m, e etc
+N_PARAMS = 2  # amount of parameters besides the four-vector
+
+DATA_SIZE = N_PARAMS + N_PARTICLES * LEN_VECTOR * N_BINS
