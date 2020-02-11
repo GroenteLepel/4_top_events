d26abbbacecb4ad09431f46aa47f37145397c636
GroenteLepel <d.kok@student.science.ru.nl>
2020-02-11 11:00:57 +0100

    Added function for setting gpu growth in order to prevent error "Could not create cudnn handle: CUDNN_STATUS_ALLOC_FAILED".

--- network_model.py
+++ network_model.py
@@ -32,3 +32,13 @@ def init_model():
                   loss='mean_squared_error',
                   metrics=['accuracy'])
     return model
+
+def set_gpu_growth():
+    """
+    If you are using the GPU that also runs the system, you don't want
+    tensorflow to commandeer the entire memory, so this should prevent that.
+    Fixes the error "Could not create cudnn handle: CUDNN_STATUS_ALLOC_FAILED"
+    """
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    tf.config.experimental.set_memory_growth(gpus[0], True)
+
