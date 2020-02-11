61067d620f004f8541c9b2e08bb47a6bc0a7917f
GroenteLepel <d.kok@student.science.ru.nl>
2020-02-11 16:58:14 +0100

    Added a 2D convolutional network to go together with the 2D array, together with a 2D concatenated model.

--- network_model.py
+++ network_model.py
@@ -2,36 +2,121 @@ import pandas as pd
 import modify_data as md
 import tensorflow as tf
 from tensorflow.keras import layers
+import config as conf
+from tensorflow.keras import models
+import ipykernel  # for the verbose output of tensorflow
 
 
-def init_model():
+def init_model_1d():
     model = tf.keras.Sequential()
-    model.add(layers.Reshape((162, 1), input_shape=(162,)
+    model.add(layers.Reshape((conf.SIZE_1D, 1), input_shape=(conf.SIZE_1D,)
                              ))
-    # model.add(layers.Conv1D(128, 48,
-    #                         activation='relu',
-    #                         padding='same',
-    #                         ))
-    # model.add(layers.MaxPool1D(2
-    #                            ))
     model.add(layers.Conv1D(64, 24,
-                            activation='relu'
-                            # data_format='channels_first',
-                            # input_shape=(1, 162),
+                            activation=tf.nn.leaky_relu
                             ))
-    # model.add(layers.MaxPool1D(2
-    #                            ))
-    # model.add(layers.Dropout(0.2))
     model.add(layers.Conv1D(32, 12,
                             activation='tanh'
                             ))
     model.add(layers.Flatten())
-    model.add(layers.Dense(16, activation='relu'))
-    model.add(layers.Dense(1, activation='sigmoid'))
+    model.add(layers.Dense(16,
+                           activation=tf.nn.leaky_relu
+                           ))
+    model.add(layers.Dense(1,
+                           activation='sigmoid'
+                           ))
     model.compile(optimizer='nadam',
-                  loss='mean_squared_error',
+                  loss='binary_crossentropy',
                   metrics=['accuracy'])
     return model
+
+
+def init_model_2d():
+    model = tf.keras.Sequential()
+    model.add(
+        layers.Reshape((conf.N_PARTICLES + 1, conf.N_BINS, conf.LEN_VECTOR),
+                       input_shape=(conf.SIZE_2D,)
+                       ))
+    model.add(layers.Conv2D(64, (1, 1),
+                            activation=tf.nn.leaky_relu
+                            ))
+    model.add(layers.Conv2D(64, (1, 1),
+                            activation=tf.nn.leaky_relu
+                            ))
+    model.add(layers.Conv2D(128, (2, 2)))
+    # model.add(layers.MaxPool2D())
+    model.add(layers.Conv2D(128, (1, 1),
+                            activation=tf.nn.leaky_relu
+                            ))
+    model.add(layers.Conv2D(128, (1, 1),
+                            activation=tf.nn.leaky_relu
+                            ))
+    model.add(layers.MaxPool2D())
+    model.add(layers.Conv2D(56, (1, 1),
+                            activation=tf.nn.leaky_relu
+                            ))
+    model.add(layers.Conv2D(56, (1, 1),
+                            activation=tf.nn.leaky_relu
+                            ))
+    # model.add(layers.MaxPool2D())
+    model.add(layers.Flatten())
+    model.add(layers.Dense(16,
+                           activation=tf.nn.leaky_relu
+                           ))
+    model.add(layers.Dense(1,
+                           activation='sigmoid'
+                           ))
+    model.compile(optimizer='nadam',
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
+    model.summary()
+
+    return model
+
+
+def init_concatenated_model():
+    # generating inputs
+    input = layers.Input(shape=(conf.SIZE_2D,))
+    reshaped = layers.Reshape(
+        (conf.N_PARTICLES + 1, conf.N_BINS, conf.LEN_VECTOR),
+        )(input)
+
+    # channel 1
+    conv1 = layers.Conv2D(64, (1, 1), activation=tf.nn.leaky_relu)(reshaped)
+    conv1 = layers.Conv2D(64, (1, 1), activation=tf.nn.leaky_relu)(conv1)
+    drop1 = layers.Dropout(0.5)(conv1)
+    pool1 = layers.MaxPool2D()(drop1)
+    flat1 = layers.Flatten()(pool1)
+
+    # channel 2
+    conv2 = layers.Conv2D(32, (conf.N_PARTICLES + 1, 1), activation=tf.nn.leaky_relu)(
+        reshaped)
+    flat2 = layers.Flatten()(conv2)
+
+    # channel 3
+    conv3 = layers.Conv2D(32, (1, conf.N_BINS), activation=tf.nn.leaky_relu)(reshaped)
+    flat3 = layers.Flatten()(conv3)
+
+    # channel 4
+    conv4 = layers.Conv2D(64, (3, 3), activation=tf.nn.leaky_relu)(reshaped)
+    pool4 = layers.MaxPool2D()(conv4)
+    flat4 = layers.Flatten()(pool4)
+
+    # merge
+    merged = layers.concatenate([flat1, flat2, flat3, flat4])
+
+    # interpretation
+    dense1 = layers.Dense(10, activation='relu')(merged)
+    outputs = layers.Dense(1, activation='sigmoid')(dense1)
+    model = models.Model(inputs=input, outputs=outputs)
+
+    # compile
+    model.compile(loss='binary_crossentropy', optimizer='nadam',
+                  metrics=['accuracy'])
+
+    # summarize
+    print(model.summary())
+    return model
+
 
 def set_gpu_growth():
     """
@@ -41,4 +126,3 @@ def set_gpu_growth():
     """
     gpus = tf.config.experimental.list_physical_devices('GPU')
     tf.config.experimental.set_memory_growth(gpus[0], True)
-
